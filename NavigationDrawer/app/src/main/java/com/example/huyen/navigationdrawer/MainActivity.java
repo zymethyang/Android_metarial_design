a5a8cafd9702839254b99dcfa10f4bfe303321ac
Tri Nguyen <zymethyang@gmail.com>
2018-10-06 00:39:18 +0700

    drawer navigation part 1

---
+++ NavigationDrawer/app/src/main/java/com/example/huyen/navigationdrawer/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.huyen.navigationdrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navigation_drawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
